f7b3331530a439a49ef26e02bdab3e9b3ecbea29
mrgamer <mister.gamer@gmail.com>
2013-08-02 17:00:20 +0200

    throttle module included in sources, that's not really necessary...

---
+++ src/helpers.js
@@ -0,0 +1,36 @@
+angular.module('helperFunctions', [])
+.factory('throttle', ['$timeout', function ($timeout) {
+  return function (delay, no_trailing, callback, debounce_mode) {
+    var timeout_id,
+    last_exec = 0;
+    
+    if (typeof no_trailing !== 'boolean') {
+      debounce_mode = callback;
+      callback = no_trailing;
+      no_trailing = undefined;
+    }
+    
+    var wrapper = function () {
+      var that = this
+        , elapsed = +new Date() - last_exec
+        , args = arguments
+        , exec = function () {
+          last_exec = +new Date();
+          callback.apply(that, args);
+        }
+        , clear = function () {
+          timeout_id = undefined;
+        };
+      
+      if (debounce_mode && !timeout_id) { exec(); }
+      if (timeout_id) { $timeout.cancel(timeout_id); }
+      if (debounce_mode === undefined && elapsed > delay) {
+        exec();
+      } else if (no_trailing !== true) {
+        timeout_id = $timeout(debounce_mode ? clear : exec, debounce_mode === undefined ? delay - elapsed : delay);
+      }
+    };
+    
+    return wrapper;
+  };
+}]);
