430660e0d282b3a800bcdb82aa59f3f1a117fcb6
mrgamer <mister.gamer@gmail.com>
2013-08-05 16:38:40 +0200

    fix stupid mistake on playPause index (it CAN be 0, since playlist is 0...n)

--- src/angular-audio-player.js
+++ src/angular-audio-player.js
@@ -96,7 +96,7 @@ angular.module('angular-audio-player', ['helperFunctions'])
         this._audioTag.play();
       },
       playPause: function (index) {
-        if (index) {
+        if (typeof index === 'number') {
           this.play(index);
         } else if (this.playing) {
           this.pause();
