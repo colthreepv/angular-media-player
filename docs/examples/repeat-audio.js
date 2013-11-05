7d7ec4fc18b36a1c2c58a0e98bb11b4d21791c32
mrgamer <mister.gamer@gmail.com>
2013-11-05 18:46:45 +0100

    feat(example): ng-repeat first push, another to follow.

---
+++ docs/examples/repeat-audio.js
@@ -0,0 +1,22 @@
+angular.module('docs')
+.controller('RepeatController', function ($scope) {
+  // Taken from http://en.wikipedia.org/wiki/List_of_songs_considered_the_best
+  // Thanks again, wikipedia.
+  var top100SongsEver = [
+    { url: 'http://upload.wikimedia.org/wikipedia/en/5/5e/U2_One.ogg', displayName: 'U2 - One' },
+    { url: 'http://upload.wikimedia.org/wikipedia/en/6/6c/NirvanaSmellsLikeTeenSpirit.ogg', displayName: 'Nirvana - Smells Like Teen Spirit' },
+    { url: 'http://upload.wikimedia.org/wikipedia/en/b/be/My_Name_Is.ogg', displayName: 'Eminem - My Name is' },
+    { url: 'http://upload.wikimedia.org/wikipedia/en/c/c4/Radiohead_-_Creep_%28sample%29.ogg', displayName: 'Radiohead - Creep' },
+    { url: 'http://upload.wikimedia.org/wikipedia/en/6/64/OasisLiveForever.ogg', displayName: 'Oasis - Live Forever' },
+    { url: 'http://upload.wikimedia.org/wikipedia/en/6/65/Eagles_-_Hotel_California.ogg', displayName: 'Eagles - Hotel California' },
+    { url: 'http://upload.wikimedia.org/wikipedia/en/c/cb/Stairway_to_Heaven_3_sections.ogg', displayName: 'Led Zeppelin - Stairway to Heaven' },
+    { url: 'http://upload.wikimedia.org/wikipedia/en/c/cb/Pink_floyd_another_brick_in_the_wall_part_2.ogg', displayName: 'Pink Floyd - Another Brick in the Wall' },
+    { url: 'http://upload.wikimedia.org/wikipedia/en/d/d0/Beatles_cometogether.ogg', displayName: 'Beatles - Come Together' },
+    { url: 'http://upload.wikimedia.org/wikipedia/en/d/db/Layla_sample_1.ogg', displayName: 'Derek and the Dominos - Layla' }
+  ];
+
+  $scope.audioPlaylist = top100SongsEver.map(function (song, index, array) {
+    var parseTitle = song.displayName.match(/(.*?)\s?-\s?(.*)?$/);
+    return { src: song.url, type: 'audio/ogg', artist: parseTitle[1], title: parseTitle[2] };
+  });
+});
---
+++ docs/examples/repeat-audio.md
@@ -0,0 +1,2 @@
+# Using ng-repeat with playlist elements
+Iterating over the playlist objects shouldn't be any different than using another _list_ object.
