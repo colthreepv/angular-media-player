ee09c6a546f03bebff3e055afb20de6584b5de8f
mrgamer <mister.gamer@gmail.com>
2013-11-06 15:56:05 +0100

    wip(examples): creating interactive example

--- docs/examples/interactive.js
+++ docs/examples/interactive.js
@@ -1,4 +1,12 @@
 angular.module('docs')
-.controller('InteractiveController', function ($scope) {
+.controller('InteractiveController', function ($scope, top100SongsEver, $log) {
   $scope.playlist1 = [];
+  $scope.prefabPlaylist = top100SongsEver.map(function (song, index, array) {
+    var parseTitle = song.displayName.match(/(.*?)\s?-\s?(.*)?$/);
+    return { src: song.url, type: 'audio/ogg', artist: parseTitle[1], title: parseTitle[2] };
+  });
+
+  $scope.addSong = function (audioElement) {
+    $log.debug('added song', song);
+  };
 });
