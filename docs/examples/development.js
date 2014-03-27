e23fa98088bee61a662ce87e5918dee437f0a397
mrgamer <mister.gamer@gmail.com>
2014-03-27 12:46:55 +0100

    Better development page, more controls, more debug!

--- docs/examples/development.js
+++ docs/examples/development.js
@@ -1,7 +1,4 @@
 angular.module('docs', ['audioPlayer'])
 .controller('DevelopmentController', function ($scope, $log) {
-  $scope.playPls = function (player) {
-    player.play();
-    $log.debug('i tried to play stuff');
-  };
+  $scope.browser = navigator.userAgent;
 });
